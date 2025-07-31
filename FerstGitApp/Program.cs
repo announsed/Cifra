360eaccd11346eea4dcbb363c917d946f6a7e88e
Sergey E <epifanov.02@mail.ru>
2025-07-31 22:02:29 +0300

    ПРактикум на статику

--- FerstGitApp/Program.cs
+++ FerstGitApp/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello");
+﻿
+Point OnePoint = new Point(8, 5);
+Point TwoPoint = new Point(3, 8);
+Console.WriteLine(Point.CounterPoint + " " + Point.LengthPoints(OnePoint, TwoPoint));
+
+
+
+class Point
+{
+    public static int CounterPoint = 0;
+
+    public readonly int X;
+    public readonly int Y;
+    public Point(int x, int y) 
+    {
+        X = x; 
+        Y = y;
+        CounterPoint++;
+    }
+
+    public static string LengthPoints(Point OnePoint, Point TwoPoint) 
+    {
+        int LenghtX = OnePoint.X - TwoPoint.X;
+        int LenghtY = TwoPoint.Y - OnePoint.Y;
+
+        return $"Расстояние между точками ({OnePoint} и {TwoPoint}): по X - {LenghtX}, по Y - {LenghtY}";
+    }
+}
