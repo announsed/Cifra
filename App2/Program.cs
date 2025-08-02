a08907d16bc682b480575b933a00af77bd670284
Sergey E <epifanov.02@mail.ru>
2025-08-02 21:36:16 +0300

    Добавлена практика по пространству имен

---
+++ App2/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace payments 
+{
+    public class Banck 
+    {
+        public virtual int Score { get; set; }
+
+        public virtual int BankCard { get; set; }
+        internal Banck(int Score, int BankCard)
+        {
+            this.Score = Score;
+            this.BankCard = BankCard;
+        }
+    }
+
+
+    class Client : Banck
+    {
+        int Score = 0;
+        int BankCard = 0;
+        int Money;
+        public Client(int Score, int BankCard, int Money) : base(Score, BankCard)
+        {
+            this.Score = Score;
+            this.BankCard = BankCard;
+            this.Money = Money;
+        }
+
+        static void PayOrder(Client client, int PaymentAmount) 
+        {
+            if (client.Money >= PaymentAmount)
+            {
+                client.Money = client.Money - PaymentAmount;
+            }
+            else 
+            {
+                Console.WriteLine(" Недостаточно средств");
+            }
+            
+        }
+        static void PaymentToAnotherAccount() 
+        {
+
+        }
+    }
+
+
+}
