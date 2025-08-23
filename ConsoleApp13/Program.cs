ed31bf7141b755ae9d747383b0f1b7b2cc5565ed
announsed <147243328+announsed@users.noreply.github.com>
2025-08-23 21:12:58 +0300

    Warning

--- ConsoleApp13/Program.cs
+++ ConsoleApp13/Program.cs
@@ -1,14 +1,36 @@
-﻿class Person 
+﻿using System.Text.Encodings.Web;
+using System.Text.Json;
+
+Person person = new Person("Паша", "Котов", 22,"ул. Пушкина, д.4а, кв. 921", "pasinto@mail.ru",new (1998, 10, 23));
+
+
+var options = new JsonSerializerOptions
+{
+    Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+    WriteIndented = true // Для читаемости, необязательно
+};
+Console.WriteLine($"{person.Name} {person.LastName} {person.Age} {person.Address} {person.Email} {person.DateOfBirth}");
+string personJSON = JsonSerializer.Serialize(person, typeof (Person), options);
+StreamWriter file = File.CreateText("C:\\Users\\Student\\source\\repos\\Sergey\\Cifra\\ConsoleApp13\\bin\\Debug\\net9.0\\json.json");
+file.WriteLine(personJSON);
+file.Close();
+
+
+class Person 
 {
     public string Name;
-    public int LastName;
+    public string LastName;
     public int Age;
     public string Address;
-    public string @mail;
-    public DateOnly dateOfBirth;
-    public Person(string name, int lastName, int age, string address, string mail, DateOnly dateOnly) 
+    public string Email;
+    public DateOnly DateOfBirth;
+    public Person(string name, string lastName, int age, string address, string mail, DateOnly dateOnly) 
     {
-
+        Name = name;
+        LastName = lastName;
+        Age = age;
+        Address = address;
+        Email = mail;
+        DateOfBirth = dateOnly;
     }
-
 }
