88df2b2194384a5b56219fe743b55918c5e93a0f
announsed <147243328+announsed@users.noreply.github.com>
2025-08-26 22:40:49 +0300

    Добавление using для options

--- ConsoleApp13/Program.cs
+++ ConsoleApp13/Program.cs
@@ -1,18 +1,11 @@
 ﻿using System.Text.Encodings.Web;
 using System.Text.Json;
-
+using JsonOption = JsonOptions.JsonOptions;
 Person person = new Person("Паша", "Котов", 22,"ул. Пушкина, д.4а, кв. 921", "pasinto@mail.ru",new (1998, 10, 23));
 
 
-var options = new JsonSerializerOptions
-{
-    Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
-    WriteIndented = true, // Для читаемости, необязательно
-    IncludeFields = true
-};
-
 Console.WriteLine($"{person.Name} {person.LastName} {person.Age} {person.Address} {person.Email} {person.DateOfBirth}");
-string personJSON = JsonSerializer.Serialize(person, typeof (Person), options);
+string personJSON = JsonSerializer.Serialize(person, JsonOption.options);
 StreamWriter file = File.CreateText("C:\\Users\\Student\\source\\repos\\Sergey\\Cifra\\ConsoleApp13\\Wuw.json");
 file.WriteLine(personJSON);
 file.Close();
@@ -36,3 +29,19 @@ class Person
         DateOfBirth = dateOnly;
     }
 }
+
+namespace JsonOptions
+{
+    public static class JsonOptions
+    {
+        public static JsonSerializerOptions options = new JsonSerializerOptions
+        {
+            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+            WriteIndented = true, // Для читаемости, необязательно
+            IncludeFields = true
+        };
+    }
+}
+
+
+
