5358a99a7b61dc029d11fc4051d821ffa02eb322
announsed <147243328+announsed@users.noreply.github.com>
2025-09-02 21:54:08 +0300

    Задачка со счётчиками

---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,44 @@
+﻿Thread thread1 = new Thread(() =>
+{
+    for (int i = 0; i < 10; i++)
+    {
+        Counter.Increase();
+    } 
+});
+Thread thread2 = new Thread(() =>
+{
+    for (int i = 0; i < 10; i++)
+    {
+        Counter.Decrease();
+    }
+});
+thread1.Start();
+thread2.Start();
+thread1.Join();
+thread2.Join();
+Console.WriteLine(" Всё завершилась");
+
+
+
+internal static class Counter
+{
+    public static object lockObject = new object();
+    public static int count = 0;
+    public static void Increase()
+    {
+        lock (lockObject)
+        {
+            count++;
+            Console.WriteLine($"Значение: {count} увеличено на 1");
+        }
+    }
+    public static void Decrease()
+    {
+        lock (lockObject)
+        {
+            count--;
+            Console.WriteLine($"Значение: {count} уменьшено на 1");
+        }
+    }
+
+}
