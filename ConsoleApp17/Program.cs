ed2c64efea546217802aa7ca760d1e6ec814b9c3
announsed <147243328+announsed@users.noreply.github.com>
2025-09-02 22:36:49 +0300

    Снежинка
    //Демонстрация организации потоков
    //Падающие объекты.
    //Реализуйте анимацию падающих объектов, например, снежинок или мячей. Каждый объект - это отдельный поток. Игрок может управлять одним из объектов, а остальные двигаются независимо.

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,31 @@
+﻿Snowflake snowflake1 = new Snowflake();
+Thread thread = new Thread(snowflake1.Launch);
+thread.Start();
+
+class Snowflake
+{
+    private char _snow = '*';
+    public int Random;
+    private void AreaRandom() 
+    {
+        Random random = new Random();
+        this.Random =  random.Next(0, Console.WindowWidth);
+    }
+    private void Movement(int top)
+    {
+        Console.Clear();
+        Console.SetCursorPosition(Random, top);
+        Console.Write(_snow);
+        Thread.Sleep(360);
+    }
+
+    public void Launch() 
+    {
+        Console.CursorVisible = false;
+        AreaRandom();
+        for (int i = 0; i < Console.WindowHeight; i++)
+        {
+            Movement(i);
+        }
+    }
+}
