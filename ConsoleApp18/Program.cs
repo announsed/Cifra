d0ad3fa8e39910fdadd998b81e9a4d27c1a1a810
announsed <147243328+announsed@users.noreply.github.com>
2025-09-04 21:46:31 +0300

    Вот тут не возникает ошибка и одна из них выполняется слишком быстро

---
+++ ConsoleApp18/Program.cs
@@ -0,0 +1,39 @@
+﻿newFileProgram.newFile МойНовенькийФайл = new newFileProgram.newFile("ТУТ ДОЛЖНА БЫТЬ ОШИБКА", "Это текст файла, он будет создан в запуске 1");
+for (int i = 0; i < 1000; i++)
+{
+    МойНовенькийФайл.FileCreate(i);
+    Thread.Sleep(123);
+}
+
+namespace newFileProgram
+{
+    public class newFile
+    {
+        private string _fileName;
+        private string _fileText;
+        public newFile(string FileName, string FileText)
+        {
+            this._fileName = @"C:\Users\Student\source\repos\Sergey\Cifra\" + FileName + ".txt";
+            this._fileText = FileText;
+        }
+        public void FileCreate(int i)
+        {
+            try
+            {
+                File.AppendAllText(this._fileName, this._fileText + $" - {i}");
+                Console.WriteLine(" Завершили создание файла!");
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine(" +==+");
+            }
+        }
+    }
+};
+
+
+
---
+++ ConsoleApp19(NewFileProgram)/Program.cs
@@ -0,0 +1,9 @@
+﻿using newFileProgram;
+
+newFile МойНовенькийФайл = new newFile("ТУТ ДОЛЖНА БЫТЬ ОШИБКА", "Это текст файла, он будет создан в запуске 2");
+
+for (int i = 0; i < 100; i++)
+{
+    МойНовенькийФайл.FileCreate(i);
+    Thread.Sleep(123);
+}
