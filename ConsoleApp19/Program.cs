27292d3d5e844be8e5b2bf2593d5f38ace9d8e67
announsed <147243328+announsed@users.noreply.github.com>
2025-09-06 21:04:31 +0300

    Задание по таскам: Параллельный ввод
    Информацию о таске он так и не даёт вывести

---
+++ ConsoleApp19/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+
+
+class Program
+{
+    static void Main()
+    {
+        // Создаем экземпляры RunLine
+        RunLine runLine1 = new RunLine();
+        RunLine runLine2 = new RunLine();
+
+        // Создаем задачи и передаем им методы экземпляров
+        Task oneTask = Task.Run(() =>
+        {
+            runLine1.Run();
+        });
+
+        Task task2 = Task.Run(() =>
+        {
+            runLine2.RunLiner();
+        });
+
+        // Ждем завершения задач
+        Task.WaitAll(oneTask, task2);
+    }
+}
+
+internal class RunLine
+{
+    public Task Task { get; set; }
+
+    internal void Run()
+    {
+        Console.WriteLine($"{GetType().FullName} {Task?.Id} Это в методе: {MethodBase.GetCurrentMethod().Name}");
+    }
+
+    internal void RunLiner()
+    {
+        Console.WriteLine($"{GetType().FullName} {Task?.Id} Это в методе: {MethodBase.GetCurrentMethod().Name}");
+    }
+}
