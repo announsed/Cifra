d3b2b2d49223da3656f890abccf88dd51ec521d3
announsed <147243328+announsed@users.noreply.github.com>
2025-09-06 21:05:37 +0300

    Рандом с тасками

---
+++ ConsoleApp20/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Task taskRundom1 = Task.Run(() =>
+{
+    Random random = new Random();
+    Console.WriteLine(random.Next(0, 256) + "    Вот это случайное число в диапазоне 0 - 255");
+});
+
+Task taskRundom2 = Task.Run(() =>
+{
+    Random random = new Random();
+    Console.WriteLine(random.Next(0, 27) + "    Вот это случайное число в диапазоне 0 - 27");
+});
+Task.WaitAll(taskRundom1, taskRundom2);
