9cdf3870b24a7d4d82cf83ed75928c82082fa0e8
announsed <147243328+announsed@users.noreply.github.com>
2025-09-06 21:44:03 +0300

    Практика на асинк

---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,25 @@
+﻿
+ var i = await Calculate(1, 7);
+Console.WriteLine(i);
+
+static Task<long> Calculate(int oneNumber, int twoNumber)
+{
+    long hashPlus = 0;
+
+    for (int i = oneNumber; i <= twoNumber; i++)
+    {
+        if ((i % 1 == 0 && i % i == 0) && i )
+        {
+            hashPlus += i;
+        }
+        else
+        {
+            continue;
+        }
+    }
+
+    return Task.Run(() =>
+    {
+        return hashPlus;
+    });
+}
