6a5c19d21e217778cd1bec34da58c0354807cd02
announsed <147243328+announsed@users.noreply.github.com>
2025-09-11 22:00:47 +0300

    Exel

---
+++ EPPlus/Program.cs
@@ -0,0 +1,21 @@
+﻿using OfficeOpenXml;
+ExcelWorksheet excelWorksheet;
+string filePath = @"C:\Users\Public\Documents\Этот Документ Создан С Помощью кода на C#.xlsx";
+ExcelPackage.License.SetNonCommercialOrganization("Корпорация Зла");
+ExcelPackage newBook = new ExcelPackage(filePath);
+List<string> listsName = new List<string>() {"Листик 1", "Листик 2", "Листик 3", "Листик 4", "Листик 5" };
+List<ExcelWorksheet> excelWorksheets = new List<ExcelWorksheet>();
+foreach (var nameWorksheet in listsName) 
+{
+    if (newBook.Workbook.Worksheets[nameWorksheet] == null)
+    {
+        excelWorksheets.Add(newBook.Workbook.Worksheets.Add(nameWorksheet));
+    }
+    else 
+    {
+        excelWorksheets.Add(newBook.Workbook.Worksheets[nameWorksheet]);
+    }
+}
+
+FileInfo fileInfo = new FileInfo(filePath);
+newBook.SaveAs(fileInfo);
