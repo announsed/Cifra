9179c3a2d5c1715f0c4a31e2f4875f3e565efd31
announsed <147243328+announsed@users.noreply.github.com>
2025-09-16 22:31:20 +0300

    Вин формы с таблицами

---
+++ WinFormsFormoshki/Form1.cs
@@ -0,0 +1,73 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private List<User> _users = new List<User>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddDataBtn_Click(object sender, EventArgs e)
+        {
+            AddUser(textBoxUserName.Text, textBoxUserSurname.Text, textBoxUserAge.Text);
+
+        }
+        private void AddUser(string name, string surName, string age)
+        {
+            _users.Add(new User(name, surName, int.Parse(age)));
+        }
+
+        private void textBoxUserName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBoxUserSurname_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxUserAge_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UploadListToGridBTN_Click(object sender, EventArgs e)
+        {
+            UploadUser();
+        }
+        private void UploadUser()
+        {
+            foreach (User user in _users)
+            {
+                dataGridView1.Rows.Add(user.Name, user.SurName, user.Age);
+            }
+        }
+
+        private void dataGridView1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete)
+            {
+                if (dataGridView1.SelectedCells.Count > 0)
+                {
+                    dataGridView1.Rows.RemoveAt(dataGridView1.SelectedCells[0].RowIndex);
+                }
+            }
+        }
+    }
+}
---
+++ WinFormsFormoshki/Program.cs
@@ -0,0 +1,31 @@
+namespace WinFormsApp1
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public int Age { get; set; }
+        public User(string name, string surName, int age)
+        {
+            Name = name;
+            SurName = surName;
+            Age = age;
+        }
+    }
+
+}
