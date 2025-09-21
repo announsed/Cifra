eac6a14c0edb0dcfb68966af6c90a1443eee9605
announsed <147243328+announsed@users.noreply.github.com>
2025-09-21 20:39:08 +0300

    (Обновление) исправлены ошибки, добавлена возможность выгрузки в CSV формат

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -28,6 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             textBox1 = new TextBox();
             button1 = new Button();
             button2 = new Button();
@@ -42,24 +44,40 @@
             textBox5 = new TextBox();
             label6 = new Label();
             dateTimePicker1 = new DateTimePicker();
+            groupBox1 = new GroupBox();
+            button5 = new Button();
+            button4 = new Button();
+            button3 = new Button();
+            dataGridView1 = new DataGridView();
+            IdUser = new DataGridViewTextBoxColumn();
+            UserName = new DataGridViewTextBoxColumn();
+            FirstName = new DataGridViewTextBoxColumn();
+            SecondName = new DataGridViewTextBoxColumn();
+            Email = new DataGridViewTextBoxColumn();
+            Password = new DataGridViewTextBoxColumn();
+            RegistrationDate = new DataGridViewTextBoxColumn();
+            BirthDate = new DataGridViewTextBoxColumn();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // textBox1
             // 
             textBox1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            textBox1.Location = new Point(359, 43);
+            textBox1.Location = new Point(314, 35);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(1410, 71);
+            textBox1.Size = new Size(709, 71);
             textBox1.TabIndex = 0;
             textBox1.TextChanged += textBox1_TextChanged;
             // 
             // button1
             // 
+            button1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
             button1.Font = new Font("Segoe UI Semibold", 18F, FontStyle.Bold, GraphicsUnit.Point, 204);
             button1.ForeColor = Color.FromArgb(0, 192, 0);
-            button1.Location = new Point(82, 740);
+            button1.Location = new Point(20, 781);
             button1.Name = "button1";
-            button1.Size = new Size(562, 183);
+            button1.Size = new Size(471, 183);
             button1.TabIndex = 1;
             button1.Text = "Зарегистрировать пользователя";
             button1.UseVisualStyleBackColor = true;
@@ -67,11 +85,12 @@
             // 
             // button2
             // 
+            button2.Anchor = AnchorStyles.Bottom;
             button2.Font = new Font("Segoe UI Semibold", 18F, FontStyle.Bold, GraphicsUnit.Point, 204);
             button2.ForeColor = Color.Maroon;
-            button2.Location = new Point(1137, 740);
+            button2.Location = new Point(536, 781);
             button2.Name = "button2";
-            button2.Size = new Size(633, 183);
+            button2.Size = new Size(419, 183);
             button2.TabIndex = 2;
             button2.Text = "Выполнить сериализацию";
             button2.UseVisualStyleBackColor = true;
@@ -81,7 +100,7 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            label1.Location = new Point(82, 43);
+            label1.Location = new Point(12, 119);
             label1.Name = "label1";
             label1.Size = new Size(271, 65);
             label1.TabIndex = 3;
@@ -92,7 +111,7 @@
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            label2.Location = new Point(82, 131);
+            label2.Location = new Point(23, 35);
             label2.Name = "label2";
             label2.Size = new Size(145, 65);
             label2.TabIndex = 4;
@@ -102,9 +121,9 @@
             // textBox2
             // 
             textBox2.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            textBox2.Location = new Point(360, 131);
+            textBox2.Location = new Point(314, 119);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(1410, 71);
+            textBox2.Size = new Size(709, 71);
             textBox2.TabIndex = 5;
             textBox2.TextChanged += textBox2_TextChanged;
             // 
@@ -112,7 +131,7 @@
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            label3.Location = new Point(82, 319);
+            label3.Location = new Point(23, 295);
             label3.Name = "label3";
             label3.Size = new Size(142, 65);
             label3.TabIndex = 6;
@@ -123,7 +142,7 @@
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            label4.Location = new Point(82, 419);
+            label4.Location = new Point(23, 387);
             label4.Name = "label4";
             label4.Size = new Size(214, 65);
             label4.TabIndex = 7;
@@ -133,18 +152,18 @@
             // textBox3
             // 
             textBox3.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            textBox3.Location = new Point(359, 319);
+            textBox3.Location = new Point(314, 295);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(1410, 71);
+            textBox3.Size = new Size(709, 71);
             textBox3.TabIndex = 8;
             textBox3.TextChanged += textBox3_TextChanged;
             // 
             // textBox4
             // 
             textBox4.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            textBox4.Location = new Point(360, 419);
+            textBox4.Location = new Point(314, 381);
             textBox4.Name = "textBox4";
-            textBox4.Size = new Size(1410, 71);
+            textBox4.Size = new Size(709, 71);
             textBox4.TabIndex = 9;
             textBox4.TextChanged += textBox4_TextChanged;
             // 
@@ -152,7 +171,7 @@
             // 
             label5.AutoSize = true;
             label5.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            label5.Location = new Point(82, 226);
+            label5.Location = new Point(20, 208);
             label5.Name = "label5";
             label5.Size = new Size(249, 65);
             label5.TabIndex = 10;
@@ -161,9 +180,9 @@
             // textBox5
             // 
             textBox5.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            textBox5.Location = new Point(359, 226);
+            textBox5.Location = new Point(314, 208);
             textBox5.Name = "textBox5";
-            textBox5.Size = new Size(1410, 71);
+            textBox5.Size = new Size(709, 71);
             textBox5.TabIndex = 11;
             textBox5.TextChanged += textBox5_TextChanged;
             // 
@@ -171,7 +190,7 @@
             // 
             label6.AutoSize = true;
             label6.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            label6.Location = new Point(82, 528);
+            label6.Location = new Point(23, 472);
             label6.Name = "label6";
             label6.Size = new Size(388, 65);
             label6.TabIndex = 12;
@@ -179,38 +198,182 @@
             // 
             // dateTimePicker1
             // 
+            dateTimePicker1.CalendarForeColor = Color.LawnGreen;
+            dateTimePicker1.CalendarTitleForeColor = Color.Green;
+            dateTimePicker1.CalendarTrailingForeColor = Color.FromArgb(0, 64, 0);
             dateTimePicker1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 204);
-            dateTimePicker1.Location = new Point(476, 528);
+            dateTimePicker1.Location = new Point(417, 472);
             dateTimePicker1.Name = "dateTimePicker1";
-            dateTimePicker1.Size = new Size(510, 71);
+            dateTimePicker1.Size = new Size(606, 71);
             dateTimePicker1.TabIndex = 13;
             dateTimePicker1.ValueChanged += dateTimePicker1_ValueChanged;
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = Color.Moccasin;
+            groupBox1.Controls.Add(button5);
+            groupBox1.Controls.Add(button4);
+            groupBox1.Controls.Add(button3);
+            groupBox1.Controls.Add(dataGridView1);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(dateTimePicker1);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(textBox5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Dock = DockStyle.Fill;
+            groupBox1.Location = new Point(0, 0);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(2803, 976);
+            groupBox1.TabIndex = 14;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // button5
+            // 
+            button5.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            button5.Font = new Font("Segoe UI Semibold", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button5.ForeColor = Color.Gray;
+            button5.Location = new Point(2042, 781);
+            button5.Name = "button5";
+            button5.Size = new Size(438, 183);
+            button5.TabIndex = 17;
+            button5.Text = "Выгрузить в CSV";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button4
+            // 
+            button4.Anchor = AnchorStyles.Bottom;
+            button4.Font = new Font("Segoe UI Semibold", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button4.ForeColor = Color.DeepSkyBlue;
+            button4.Location = new Point(1565, 781);
+            button4.Name = "button4";
+            button4.Size = new Size(438, 183);
+            button4.TabIndex = 16;
+            button4.Text = "Выгрузить в Excel";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button3
+            // 
+            button3.Anchor = AnchorStyles.Bottom;
+            button3.Font = new Font("Segoe UI Semibold", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.Location = new Point(1010, 781);
+            button3.Name = "button3";
+            button3.Size = new Size(509, 183);
+            button3.TabIndex = 15;
+            button3.Text = "Выгрузить в таблицу";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.BackgroundColor = Color.DarkOrange;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { IdUser, UserName, FirstName, SecondName, Email, Password, RegistrationDate, BirthDate });
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = Color.RosyBrown;
+            dataGridViewCellStyle1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            dataGridViewCellStyle1.ForeColor = Color.Teal;
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = Color.Olive;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.False;
+            dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            dataGridView1.Location = new Point(1073, 38);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 82;
+            dataGridView1.Size = new Size(1684, 508);
+            dataGridView1.TabIndex = 14;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // IdUser
+            // 
+            IdUser.HeaderText = "Id";
+            IdUser.MinimumWidth = 10;
+            IdUser.Name = "IdUser";
+            IdUser.ReadOnly = true;
+            IdUser.Width = 200;
+            // 
+            // UserName
+            // 
+            UserName.HeaderText = "Ник";
+            UserName.MinimumWidth = 10;
+            UserName.Name = "UserName";
+            UserName.ReadOnly = true;
+            UserName.Width = 200;
+            // 
+            // FirstName
+            // 
+            FirstName.HeaderText = "Имя";
+            FirstName.MinimumWidth = 10;
+            FirstName.Name = "FirstName";
+            FirstName.ReadOnly = true;
+            FirstName.Width = 200;
+            // 
+            // SecondName
+            // 
+            SecondName.HeaderText = "Фамилия";
+            SecondName.MinimumWidth = 10;
+            SecondName.Name = "SecondName";
+            SecondName.ReadOnly = true;
+            SecondName.Width = 200;
+            // 
+            // Email
+            // 
+            Email.HeaderText = "Email";
+            Email.MinimumWidth = 10;
+            Email.Name = "Email";
+            Email.ReadOnly = true;
+            Email.Width = 200;
+            // 
+            // Password
+            // 
+            Password.HeaderText = "Пароль";
+            Password.MinimumWidth = 10;
+            Password.Name = "Password";
+            Password.ReadOnly = true;
+            Password.Width = 200;
+            // 
+            // RegistrationDate
+            // 
+            RegistrationDate.HeaderText = "Дата регистрации";
+            RegistrationDate.MinimumWidth = 10;
+            RegistrationDate.Name = "RegistrationDate";
+            RegistrationDate.ReadOnly = true;
+            RegistrationDate.Width = 200;
+            // 
+            // BirthDate
+            // 
+            BirthDate.HeaderText = "Дата рождения";
+            BirthDate.MinimumWidth = 10;
+            BirthDate.Name = "BirthDate";
+            BirthDate.ReadOnly = true;
+            BirthDate.Width = 200;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1935, 976);
-            Controls.Add(dateTimePicker1);
-            Controls.Add(label6);
-            Controls.Add(textBox5);
-            Controls.Add(label5);
-            Controls.Add(textBox4);
-            Controls.Add(textBox3);
-            Controls.Add(label4);
-            Controls.Add(label3);
-            Controls.Add(textBox2);
-            Controls.Add(label2);
-            Controls.Add(label1);
-            Controls.Add(button2);
-            Controls.Add(button1);
-            Controls.Add(textBox1);
+            ClientSize = new Size(2803, 976);
+            Controls.Add(groupBox1);
             ForeColor = Color.Teal;
+            Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Form1";
-            Text = "Зарегистрировать, проверить, сериализовать";
+            Text = "Зарегистрировать, проверить, сериализовать, выгрузить";
             Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -229,5 +392,18 @@
         private TextBox textBox5;
         private Label label6;
         private DateTimePicker dateTimePicker1;
+        private GroupBox groupBox1;
+        private Button button3;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn IdUser;
+        private DataGridViewTextBoxColumn UserName;
+        private DataGridViewTextBoxColumn FirstName;
+        private DataGridViewTextBoxColumn SecondName;
+        private DataGridViewTextBoxColumn Email;
+        private DataGridViewTextBoxColumn Password;
+        private DataGridViewTextBoxColumn RegistrationDate;
+        private DataGridViewTextBoxColumn BirthDate;
+        private Button button4;
+        private Button button5;
     }
 }
--- WinFormsApp1/Program.cs
+++ WinFormsApp1/Program.cs
@@ -1,8 +1,9 @@
-using System;
-using System.IO;
-using System.Text;
+using System.Globalization;
 using System.Text.Encodings.Web;
 using System.Text.Json;
+using CsvHelper;
+using CsvHelper.Configuration;
+using OfficeOpenXml;
 
 namespace WinFormsApp1
 {
@@ -14,6 +15,7 @@ namespace WinFormsApp1
         [STAThread]
         static void Main()
         {
+            ExcelPackage.License.SetNonCommercialPersonal(" PersonalNotLicenseCommerrcial");
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
@@ -21,7 +23,7 @@ namespace WinFormsApp1
         }
     }
 
-
+    // Реализация класса User по заданию
     public class User
     {
         public int Id { get; set; }
@@ -34,37 +36,43 @@ namespace WinFormsApp1
         public DateTime BirthDate { get; set; }
     }
 
+
+    // Реализация статического класса для регистрации юзеров
     public static class UserRegistration
     {
+        // Метод для регистрации пользвателей с проверками уникальности по нику и емайл
         public static void RegisterUser(User newUser)
         {
-            for (int i = 0; i < StaticListUsers.users.Count; i++)
+            if (StaticListUsers.users.Count == 0)
+            {
+                StaticListUsers.users.Add(newUser);
+                MessageBox.Show($" Пользователь зарегистрирован");
+            }
+            else
             {
                 bool privateInfoContinue = false;
-                if (StaticListUsers.users[i].UserName != newUser.UserName)
+                for (int i = 0; i < StaticListUsers.users.Count; i++)
                 {
-                    privateInfoContinue = true;
+                    if (StaticListUsers.users[i].UserName == newUser.UserName)
+                    {
+                        privateInfoContinue = true;
+                        MessageBox.Show($" Пользователь с именем {newUser.UserName} уже существует");
+                    }
+                    else if (StaticListUsers.users[i].Email == newUser.Email)
+                    {
+                        privateInfoContinue = true;
+                        MessageBox.Show($" Пользователь c mail {newUser.Email} уже существует");
+                    }
                 }
-                else 
-                {
-                    privateInfoContinue = false;
-                    MessageBox.Show($" Пользователь с именем {newUser.UserName} уже существует");
-                }
-                if (StaticListUsers.users[i].Email != newUser.Email) 
-                {
-                    privateInfoContinue = true;
-                }
-                else
-                {
-                    privateInfoContinue = false;
-                    MessageBox.Show($" Пользователь c mail {newUser.Email} уже существует");
-                }
-                if (privateInfoContinue == true) 
+                if (privateInfoContinue != true)
                 {
                     StaticListUsers.users.Add(newUser);
+                    MessageBox.Show($" Пользователь зарегистрирован");
                 }
             }
         }
+
+        // Для сериализации юзеров из листа по заданию, с опциями для правильного отображения
         public static string SerializeUserToJson(List<User> users)
         {
             var options = new JsonSerializerOptions
@@ -78,8 +86,99 @@ namespace WinFormsApp1
         }
     }
 
+
+    // Статический класс с реализаций листа с юзерами для хранения зарегестрированных пользователей
     public static class StaticListUsers
     {
         public static List<User> users = new List<User>();
+    }
+
+    // Класс для проверки на пробелы и null
+    public static class IsNullAndAllSpaces
+    {
+        public static bool IsNullOrEmptyOrAllSpaces(string str)
+        {
+            if (str == null || str.Length == 0)
+            {
+                return true;
+            }
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (!Char.IsWhiteSpace(str[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+
+    // (EPPLus)  Создание книги, путь и имя для сохранения файла
+    public static class StaticExcelPackegeBook
+    {
+        public static ExcelPackage newBook = new ExcelPackage();
+        public static string PathInBook(string path)
+        {
+            return path;
+        }
+        public static string NameFailToSave(string pathInBook, string nameFile)
+        {
+            return pathInBook + @"\" + nameFile + ".xlsx";
+        }
+        public static void UserInputNameToListInBook(string nameList, out ExcelWorksheet excelWorksheet)
+        {
+            excelWorksheet = null;
+            if (newBook.Workbook.Worksheets[nameList] == null)
+            {
+                newBook.Workbook.Worksheets.Add(nameList);
+                excelWorksheet = newBook.Workbook.Worksheets[nameList];
+            }
+            else
+            {
+                MessageBox.Show($" Файл с именем: {nameList} не может быть создан");
+            }
+        }
+        public static void ConvertListToExcelAndFinalFileSave(ExcelWorksheet excelWorksheet, string fullPathFileSave)
+        {
+            for (int i = 0; i < StaticListUsers.users.Count; i++)
+            {
+                string[] hashUser =
+                [
+                    StaticListUsers.users[i].Id.ToString(),
+                    StaticListUsers.users[i].UserName,
+                    StaticListUsers.users[i].FirstName,
+                    StaticListUsers.users[i].SecondName,
+                    StaticListUsers.users[i].Email,
+                    StaticListUsers.users[i].Password,
+                    StaticListUsers.users[i].RegistrationDate.ToString(),
+                    StaticListUsers.users[i].BirthDate.ToString(),
+                ];
+                for (int j = 0; j < hashUser.Length; j++)
+                {
+                    excelWorksheet.Cells[i + 1, j + 1].AutoFitColumns();
+                    excelWorksheet.Cells[i + 1, j + 1].Value = hashUser[j];
+                }
+            }
+            newBook.SaveAs(new FileInfo(fullPathFileSave));
+        }
+    }
+    public static class StaticPackegeCSVFile
+    {
+        public static string PathInBook(string path)
+        {
+            return path;
+        }
+        public static string NameFailToSave(string pathInBook, string nameFile)
+        {
+            return pathInBook + @"\" + nameFile + ".csv";
+        }
+
+        public static CsvWriter StreamWriterAndCSVWriter(string path)
+        {
+            return new CsvWriter(new StreamWriter(path), new
+ CsvConfiguration (CultureInfo.InvariantCulture));
+        }
     }
 }
