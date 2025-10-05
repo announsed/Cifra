37936a8c4bb33273c1ba6049878afbe49eeead51
announsed <147243328+announsed@users.noreply.github.com>
2025-10-05 12:58:42 +0300

    Урок 70. Методы
    Урок 70. Методы

---
+++ Задания/ConsoleAppDZ25/Program.cs
@@ -0,0 +1,52 @@
+﻿internal class Numbers
+{
+    internal long CalculateFactorial(int number)
+    {
+        if (number == 1) return 1;
+
+        return number * CalculateFactorial(number - 1);
+    }
+
+    internal string ConvertToBinary(int number)
+    {
+        long[] hashArray = new long[number];
+        for (int i = 0; i < number; i++)
+        {
+            if (number >= 0)
+            {
+                hashArray[i] = number % 2;
+                number = number / 2;
+            }
+            break;
+        }
+        string str = "";
+        foreach (var num in hashArray) 
+        {
+            str += num.ToString();
+        }
+        return str;
+    }
+
+    internal long ConvertToHex(int number) 
+    {
+        return Convert.ToInt64(Convert.ToString(number, 16).ToUpper());
+    }
+
+    internal long CalculatePower(long footing, int degree)
+    {
+        if (degree == 0)
+        {
+            return 1;
+        }
+
+        if (degree % 2 == 0)
+        {
+            var p = CalculatePower(footing, degree / 2);
+            return p * p;
+        }
+        else
+        {
+            return footing * CalculatePower(footing, degree - 1);
+        }
+    }
+}
